e0dbc545bbc578bf5b2d85b6a36763259150e59e
evavanbakel <90251004+evavanbakel@users.noreply.github.com>
2021-10-06 17:45:00 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-// JavaScript Document
+//var icon = document.getElementById("zon");
+
+//zon.onclick = function(){
+  //  document.body.classList.toggle("dark-theme");
+    //if(document.body.classList.toggle("dark-theme")){
+     //   zon.src = "images/zon.png"
+    //}else{
+    //    zon.src = "/images/maan.png"
+   // }
+//}
+function lightMode(){
+    var element = document.body;
+     element.classList.toggle("light-mode");
+   }
