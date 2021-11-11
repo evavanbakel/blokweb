18e5d330fc16ffe7fad200e95e333307a82c03a7
evavanbakel <90251004+evavanbakel@users.noreply.github.com>
2021-11-11 13:54:48 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,72 @@
     //    zon.src = "/images/maan.png"
    // }
 //}
-function lightMode(){
-    var element = document.body;
-     element.classList.toggle("light-mode");
-   }
+
+
+//function lightMode(){
+ //   var element = document.body;
+ //    element.classList.toggle("light-mode");
+ //  }
+ 
+ /*----------------------------------------------------------*/
+
+ var hamburgerMenu = document.querySelector(".menu");
+ 
+ var menuButton = document.querySelector(".icon");
+
+ var sluitButton = document.querySelector(".kruis");
+
+// var zoekMenu = document.querySelector(".search");
+
+// var zoekButton = document.querySelector(".zoekicon");
+
+// var sluitZoeken = document.querySelector(".kruisje");
+
+
+ // Click on menu Button
+ 
+ menuButton.addEventListener("click", toggleHamburgerMenu);
+ 
+ 
+ 
+ function toggleHamburgerMenu(){
+ 
+     hamburgerMenu.classList.add("visible");
+ 
+ }
+ 
+ // sluiten van hamburgermenu met .remove
+ sluitButton.addEventListener("click", sluitHamburgermenu);
+ 
+ 
+ 
+ function sluitHamburgermenu(){
+ 
+     hamburgerMenu.classList.remove("visible");
+ 
+ }
+
+ /*----------------------------------------------------------------------*/ 
+//  // Voor zoekpagina
+ 
+//  zoekButton.addEventListener("click", toggleZoekpagina);
+ 
+ 
+ 
+//  function toggleZoekpagina(){
+ 
+//      zoekMenu.classList.add("aanwezig");
+ 
+//  }
+ 
+//  // sluiten van zoekpagina met .remove
+//  sluitZoeken.addEventListener("click", sluitZoekpagina);
+ 
+ 
+ 
+//  function sluitZoekpagina(){
+ 
+//      zoekMenu.classList.remove("aanwezig");
+ 
+//  }
+ 
